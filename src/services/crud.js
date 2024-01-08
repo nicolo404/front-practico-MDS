f3fffe8ef6acfc92b1b1b57c868624f3353905f2
Nicolas Silva (Talcahuano) <nsilva@marinadelsol.cl>
2024-01-08 15:18:59 -0300

    Creacion de user con formulario

--- src/services/crud.js
+++ src/services/crud.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
+const baseURL = "http://localhost:3001/api/";
+
 const getData = async () => {
-  const { data } = await axios.get("http://localhost:3001/api/obtener-usuarios/");
+  const { data } = await axios.get(baseURL+"obtener-usuarios/");
   return data;
 }
 
 const deleteData = async (id) => {
-  const { data } = await axios.delete(`http://localhost:3001/api//eliminar-usuario/${id}`);
+  const { data } = await axios.delete(`${baseURL}eliminar-usuario/${id}`);
   return data;
 }
 
-module.exports = { getData, deleteData };
+const createData = async (user) => {
+  const { user: response } = await axios.post("http://localhost:3001/api/crear-usuario/", user);
+  return response;
+}
+
+
+export { getData, deleteData, createData };
