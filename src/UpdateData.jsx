c37b148cbaa803d74d78b15ad119256b16e5fb13
Nicolas Silva (Talcahuano) <nsilva@marinadelsol.cl>
2024-01-09 16:38:42 -0300

    update 2.0

--- src/UpdateData.jsx
+++ src/UpdateData.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import {updateData} from "./services/crud";
-import { useParams } from "react-router-dom";
+import {updateData, getDataById} from "./services/crud";
+import { Link, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 const UpdateData = () => {
     const { id } = useParams();
     console.log(id);
+
     const [data, setData] = React.useState(null);
 
+    React.useEffect(() => {
+        getDataById(id).then((response) => {
+            console.log(response);
+            setData(response);
+        });
+    }, [id]);
+    console.log(data);
     const handleInputChange = (event) => {
         setData({
           ...data,
           [event.target.name]: event.target.value,
         });
-      }
+      }                                                                                                                                                                                                     
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -24,19 +32,22 @@ const UpdateData = () => {
             }, 1200);
         });
       } 
-      
+    if (!data) return null;     
+    console.log(data.nombre);                                                                                                     
     return (
         <>
             <form action="" method="post">
+                <Link to="/">Volver</Link>
                 <label htmlFor="">Nuevo Nombre</label>
-                <input type="text" name="nombre" id="nombre" onChange={handleInputChange}/>
+                <input type="text" name="nombre" id="nombre" onChange={handleInputChange} value={data.nombre}/>
                 <label htmlFor="">Nueva Edad</label>
-                <input type="number" name="edad" id="edad" onChange={handleInputChange}/>
+                <input type="number" name="edad" id="edad" onChange={handleInputChange} value={data.edad}/>
                 <label htmlFor="">Nuevo Correo</label>
-                <input type="email" name="email" id="email" onChange={handleInputChange}/>
+                <input type="email" name="email" id="email" onChange={handleInputChange} value={data.email}/>
                 <button type="submit" onClick={handleSubmit}>Actualizar Usuario</button>
             </form>
         </>
     );
     }
+
 export default UpdateData;
