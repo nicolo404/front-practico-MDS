eed9b482f9ffb354838ee38d46391fe5c75475b3
Nicolas Silva (Talcahuano) <nsilva@marinadelsol.cl>
2024-01-11 16:18:02 -0300

    update tbl_usuario listo front

--- src/services/tbl_usuario_crud.js
+++ src/services/tbl_usuario_crud.js
@@ -6,7 +6,10 @@ const get_tbl_usuario = async () => {
     const { data } = await axios.get(baseURL+"tbl_usuario/");
     return data;
 }
-
+const get_tbl_usuarioById = async (id) => {
+    const { data } = await axios.get(`${baseURL}tbl_usuario/${id}`);
+    return data;
+}
 const delete_tbl_usuario = async (id) => {
     const { data } = await axios.delete(`${baseURL}delete-tbl_usuario/${id}`);
     return data;
@@ -15,6 +18,12 @@ const delete_tbl_usuario = async (id) => {
 const create_tbl_usuario = async (tbl_usuario) => {
     const { tbl_usuario: response } = await axios.post(`${baseURL}crear-tbl_usuario`, tbl_usuario);
     return response;
-}   
+}
+const update_tbl_usuario = async (id, tbl_usuario) => {
+    const { tbl_usuario: response } = await axios.put(`${baseURL}update-tbl_usuario/${id}`, tbl_usuario);
+    return response;
+}
 
-export { get_tbl_usuario, delete_tbl_usuario, create_tbl_usuario};
+export { get_tbl_usuario, get_tbl_usuarioById , delete_tbl_usuario, create_tbl_usuario, update_tbl_usuario};
+
+
